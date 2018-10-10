cc93ec3212da20f8418c13ce199385a5d62125c8
Nate Scherer <376408+natescherer@users.noreply.github.com>
2018-10-10 11:27:25 -0400

    Improve formatting

--- NewScriptModule/src/Module.psm1
+++ NewScriptModule/src/Module.psm1
@@ -1,33 +1,39 @@
-# Implement your module commands in this script.
-
-
-# Export only the functions using PowerShell standard verb-noun naming.
-# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
-# This improves performance of command discovery in PowerShell.
-Export-ModuleMember -Function *-*
-
-function Get-ExampleFunction {
-        <#
+function New-ExampleFunction {
+    <#
     .SYNOPSIS
-        A brief description of the function or script.
+        A brief description of the function.
 
     .DESCRIPTION
         A longer description.
 
     .INPUTS
-        Description of objects that can be piped to the script
+        Description of objects that can be piped to the function
 
     .OUTPUTS
-        Description of objects that are output by the script
+        Description of objects that are output by the function
 
     .EXAMPLE
-        Example of how to run the script
+        Example of how to run the function
 
     .LINK
         Links to further documentation
 
     .NOTES
-        Detail on what the script does, if this is needed
-
+        Detail on what the function does, if this is needed
     #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$false)]
+        # Detail what this parameter does
+        [string]$Param
+    )
+
+    process {
+        # Put function code here
+    }
 }
+
+# Export only the functions using PowerShell standard verb-noun naming.
+# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
+# This improves performance of command discovery in PowerShell.
+Export-ModuleMember -Function New-ExampleFunction
