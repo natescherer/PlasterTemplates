4242b2d6d35c8c84cb0227113680931343082b4d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2018-10-18 16:59:18 -0400

    Improve module handling

--- NewScriptModule/test/Module.T.ps1
+++ NewScriptModule/test/Module.T.ps1
@@ -1,4 +1,7 @@
 $ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
+$ModulePath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psm1"
+Get-Module -Name $ModuleName -All | Remove-Module -Force -ErrorAction Ignore
+Import-Module -Name $ModulePath -Force -ErrorAction Stop
 
 InModuleScope $ModuleName {
     $ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
