58a8d6e652e15b61061b5c689d0620d73cf13e34
Nate Scherer <376408+natescherer@users.noreply.github.com>
2018-10-18 10:25:56 -0400

    Improve default module tests

--- NewScriptModule/test/Module.T.ps1
+++ NewScriptModule/test/Module.T.ps1
@@ -1,9 +1,14 @@
-$ModuleManifestName = '<%=$PLASTER_PARAM_ModuleName%>.psd1'
-$ModuleManifestPath = "$PSScriptRoot\..\src\$ModuleManifestName"
+$ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
 
-Describe 'Module Manifest Tests' {
-    It 'Passes Test-ModuleManifest' {
-        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
-        $? | Should Be $true
+InModuleScope $ModuleName {
+    $ModuleName = Split-Path -Path ($PSCommandPath -replace '\.Tests\.ps1$','') -Leaf
+    $ModulePath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psm1"
+    $ModuleManifestPath = "$(Split-Path -Path $PSScriptRoot -Parent)\src\$ModuleName.psd1"
+
+    Describe 'Module Manifest Tests' {
+        It 'Passes Test-ModuleManifest' {
+            Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+            $? | Should Be $true
+        }
     }
 }
